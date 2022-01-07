283c4f504831003d66d6f05953364215386f1b49
Pol Piella Abadia <hi@polpiella.dev>
2022-01-07 13:33:47 +0100

    Allows handling of output error pipes

--- Sources/CompilerLambda/main.swift
+++ Sources/CompilerLambda/main.swift
@@ -13,19 +13,28 @@ enum Shell {
     static func execute(command: String, args: [String]) throws -> String {
         let process = Process()
         let outputPipe = Pipe()
+        let errorPipe = Pipe()
         
         process.executableURL = URL(fileURLWithPath: command)
         process.arguments = args
         process.standardOutput = outputPipe
+        process.standardError = errorPipe
         
         try process.run()
         
-        let data = try outputPipe.fileHandleForReading.readToEnd()!
+        let outputData = try outputPipe.fileHandleForReading.readToEnd()
+        let errorData = try errorPipe.fileHandleForReading.readToEnd()
         
         process.waitUntilExit()
-
         
-        return String(data: data, encoding: .utf8)!
+        // TODO: - Better error handling
+        if let data = outputData, let outputString = String(data: data, encoding: .utf8) {
+            return outputString
+        } else if let data = errorData, let errorString = String(data: data, encoding: .utf8) {
+            return errorString
+        } else {
+            return "Could not read any of the output pipes"
+        }
     }
 }
 
